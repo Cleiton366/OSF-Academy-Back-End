b6a9be2b3d833bbd1e1d93816c2016694068b278
Cleiton366 <cleiton0050cf@gmail.com>
2022-01-09 17:23:37 -0300

    product page finished

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -1,37 +1,54 @@
-$(document).ready(async function (){
-    if(sessionStorage.getItem("token")){
-        $("#signinLi").hide();
-        $("#signupLi").hide();
-        $("#signoutLi").show();
-        $("#profileLi").show();
-        $("#cartLi").show();
-        $("#signoutLi").show();
-        $("#wishlistLi").show();
-      }else {
-        $("#signinLi").show();
-        $("#signupLi").show();
-        $("#signoutLi").hide();
-        $("#profileLi").hide();
-        $("#cartLi").hide();
-        $("#signoutLi").hide();
-        $("#wishlistLi").hide();
-      }
+$(document).ready(async function () {
+  if (sessionStorage.getItem("token")) {
+    $("#signinLi").hide();
+    $("#signupLi").hide();
+    $("#signoutLi").show();
+    $("#profileLi").show();
+    $("#cartLi").show();
+    $("#signoutLi").show();
+    $("#wishlistLi").show();
+  } else {
+    $("#signinLi").show();
+    $("#signupLi").show();
+    $("#signoutLi").hide();
+    $("#profileLi").hide();
+    $("#cartLi").hide();
+    $("#signoutLi").hide();
+    $("#wishlistLi").hide();
+  }
 });
 
 $("#wishlistBtn").click(function () {
-  if(sessionStorage.getItem("token")){
-    console.log("logado")
+  if (sessionStorage.getItem("token")) {
+    console.log("logado");
   } else window.location.href = "/signin";
 });
 
 $("#cartBtn").click(function () {
-  if(sessionStorage.getItem("token")){
-    console.log("logado")
+  if (sessionStorage.getItem("token")) {
+    console.log("logado");
   } else window.location.href = "/signin";
 });
 
 $("#buyBtn").click(function () {
-  if(sessionStorage.getItem("token")){
-    console.log("logado")
+  var token = sessionStorage.getItem("token");
+  if (sessionStorage.getItem("token")) {
+    console.log("logado");
   } else window.location.href = "/signin";
+});
+
+$(".fa-minus").click(function () {
+  var qtd = $(".quantity").html();
+  if (qtd > 1) {
+    qtd--;
+    $(".quantity").html(qtd);
+  } else {
+    window.alert("Quantity must be equal or greater than 1");
+  }
+});
+
+$(".fa-plus").click(function () {
+  var qtd = $(".quantity").html();
+  qtd++;
+  $(".quantity").html(qtd);
 });
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -3,7 +3,7 @@ const express = require("express");
 const productsController = require("../controllers/ProductsController");
 const router = express.Router();
 
-//get product
+
 router.get("/product/:id", async function (req, res) {
     const product =  await productsController.searchProductsById(req, res);
     res.render("../public/views/product.ejs", {product});
